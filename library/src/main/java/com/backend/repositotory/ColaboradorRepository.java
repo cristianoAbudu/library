bde1427834a94c856b6896d2fc6d6c98ab8a10d4
jovemTranquilao <jovem@hocem.com>
2023-09-06 20:54:30 -0300

    findByNome

--- library/src/main/java/com/backend/repositotory/ColaboradorRepository.java
+++ library/src/main/java/com/backend/repositotory/ColaboradorRepository.java
@@ -1,10 +1,14 @@
 package com.backend.repositotory;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.entity.ColaboradorEntity;
 
 public interface ColaboradorRepository extends JpaRepository<ColaboradorEntity, Integer> {
+
+	Optional<ColaboradorEntity> findByNome(String username);
 
 }
