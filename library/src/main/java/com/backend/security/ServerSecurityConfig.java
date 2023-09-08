fe0b6bd0cb959e30894682d5911e748c637c7945
jovemTranquilao <jovem@hocem.com>
2023-09-08 19:56:47 -0300

    Habilitado CORS - falta fazer aceitar o token

--- library/src/main/java/com/backend/security/ServerSecurityConfig.java
+++ library/src/main/java/com/backend/security/ServerSecurityConfig.java
@@ -3,10 +3,12 @@ package com.backend.security;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -30,5 +32,32 @@ public class ServerSecurityConfig  {
        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
        return jwtAuthenticationConverter;
    }
-
+   /*
+   @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+           .cors(AbstractHttpConfigurer::disable)
+           .csrf(AbstractHttpConfigurer::disable)
+           .authorizeHttpRequests(request -> request
+                   .requestMatchers("*").permitAll()
+                   .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                   //.requestMatchers("*").hasAuthority("SYSTEM_ADMIN")
+                   .requestMatchers("*").authenticated()
+           )
+           //.sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+           //.authenticationProvider(authenticationProvider())
+           //.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+           ;
+       ;
+       SecurityFilterChain chain = http.build();
+       //log.info("Configured security filter chain: {}",chain);
+       return chain;
+   }
+   */
+   
+   @Bean
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+   	http.cors(cors -> cors.disable());
+   	return http.build();
+   }
 }
